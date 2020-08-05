0dff678d34cad734470a3eb3fa43ec9ffdd5503f
Sobedan <sobe123sri@gmail.com>
2020-08-05 23:06:37 +0530

    Updates

---
+++ precache-manifest.67175636f8b2ea4b3a9d7770eb90b94d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee24534d12b3a74105cbfbe1d31e41e7",
+    "url": "/ususlector/index.html"
+  },
+  {
+    "revision": "ad898c8ec114265a7a97",
+    "url": "/ususlector/static/css/main.d8e7f913.chunk.css"
+  },
+  {
+    "revision": "e97817b075364c2b11ce",
+    "url": "/ususlector/static/js/2.ed7f3dc0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ususlector/static/js/2.ed7f3dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad898c8ec114265a7a97",
+    "url": "/ususlector/static/js/main.0516259a.chunk.js"
+  },
+  {
+    "revision": "c8bd50d0ef735cf52a6c",
+    "url": "/ususlector/static/js/runtime-main.45462624.js"
+  }
+]);
